b8227d5d2c154a7106328eeca50341b08e436cfb
/ SL MaLiYa / <85741405+SL-Alpha-X@users.noreply.github.com>
2021-11-05 11:43:34 +0530

    QR-GEN v.2 🌏

---
+++ CODE.sh
@@ -0,0 +1,18 @@
+clear
+apt update && apt upgrate
+clear
+apt install nodejs --fix-missing -qq -yy
+clear
+pkg install git -qq -y
+clear
+npm install axios
+clear
+npm install chalk
+clear
+npm install @adiwajshing/baileys
+clear
+git clone https://github.com/SL-Alpha-X/Alpha-X-WA-Bot-QR
+clear
+cd Alpha-X-WA-Bot-QR
+clear
+node index.js
